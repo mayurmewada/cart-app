c8b1206be0732c847564aade613782b01319eff6
Mayur Mewada <mayurmewada00@gmail.com>
2021-08-13 16:02:53 +0530

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar{" "}
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Navbar;
